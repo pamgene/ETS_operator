a42099fbd78ecc33e09622c9e8e7b8d2bba98af9
ginberg <info@gerinberg.com>
2021-05-06 10:55:42 +0000

    update operator

--- workspace.R
+++ workspace.R
@@ -1,67 +1,101 @@
 library(tercen)
+library(plyr)
 library(dplyr)
+#library(pgMulticore)
 
 options("tercen.workflowId" = "119e20e1121a1ac50b16db38b0000a3d")
 options("tercen.stepId"     = "c4810b2f-8eae-466f-937a-29438913fb63")
 
-do.ETS <- function(df){
+# linreg for exposure time scaling
+operatorFunction <- function(aFrame){
+  isOut = aFrame$bOut
+  dataX = aFrame$x
+  dataY = aFrame$y
   
-  dataX = df$.x
-  dataY = df$.y
-  
-  nPoints = nrow(df)
-  
-  if (nPoints > 1) {
-    
-    aLm <- try(lm(dataY ~ dataX + 0), silent = TRUE)
-    
+  nPoints = sum(!isOut)
+  dataX = dataX[!isOut]
+  dataY = dataY[!isOut]
+  if (nPoints > 1){      
+    aLm <- try(lm(dataY ~ dataX+0), silent = TRUE)
     if(!inherits(aLm, 'try-error')){
-      
-      slope <- aLm$coefficients[[1]]
-      intercept <- 0
-      ssY <- sum((dataY - mean(dataY)) ^ 2)
+      slope = aLm$coefficients[[1]]
+      intercept = 0
+      ssY <- sum((dataY-mean(dataY))^2)
       yFit <- predict(aLm)
-      R2 <- 1 - (sum((dataY - yFit) ^ 2) / ssY)
+      R2 <- 1-(sum((dataY-yFit)^2) / ssY)
       Result = 1;
       
     } else {
-      
       slope = NaN
       intercept = NaN
       R2 = NaN
       Result = 0;
-      
     }
-  }
-  
-  if (nPoints == 1) {
-    
-    slope = dataY / dataX
+  } 
+  if (nPoints == 1){
+    slope = dataY/dataX
     intercept = 0;
     R2 = 1
     Result = 1
-    
   }
-  
-  if (nPoints == 0) {
-    
+  if (nPoints == 0){
     slope = NaN
     intercept = NaN
     R2 = NaN
     Result = 0;
-    
   }
+  return (c(rowSeq = aFrame$rowSeq[1], colSeq = aFrame$colSeq[1], slope=slope, intercept = intercept, R2 = R2, nPoints = nPoints, Result = Result))
+}
+
+curveFitOperatorFunction <- function(dataX , result, propList) {
+  a <- result[1]
+  b <- result[2]
+  dataY <-lapply(dataX ,  function(x) { y = a*x + b})
+  return (dataY)
+}
+calFractionPresent <- function(aFrame){
+  aFrame = data.frame(aFrame, fractionPresent = sum(aFrame$Result)/length(aFrame$Result))
+  return(aFrame)
+}
+
+getPropertyValue <- function(properties=properties, name=name, prop.is.numeric = FALSE) {
+  for ( i in 1:length(properties) ) {
+    if (properties[[i]][1] == name){
+      val = properties[[i]][2]
+      if (prop.is.numeric){
+        return(as.numeric(val))
+      } else {
+        return(val)
+      }
+    }
+  }
+  stop(paste("property not found: ", name))
+  return (NULL)
+}
+
+do.ETS <- function(data) {
+  #aResult = ddply(aD, ~rowSeq + colSeq, .fun = operatorFunction, .progress=progress_win(title="Exposure Time Scaling"))
+  # bMultiCore = getPropertyValue(properties, "Multicore processing") == "Yes"
+  # if(bMultiCore){
+  #   aResult = doMultiCore(~rowSeq + colSeq, data = aD, operatorFunction = operatorFunction, .export = "operatorFunction") 
+  # } else {
+  #   aResult = ddply(aD, ~rowSeq + colSeq, .fun = operatorFunction, .progress=progress_win(title="Exposure Time Scaling"))
+  # }
   
-  return(data.frame(
-    .ri = df$.ri[1],
-    .ci = df$.ci[1],
-    slope = slope,
-    intercept = intercept,
-    R2 = R2,
-    nPoints = nPoints,
-    Result = Result
-  ))
+  metaData    <- varMetadata(data)
+  aXaxisLabel <- colnames(pData(data))[metaData$groupingType=='xAxis']  
+  aD          <- data.frame(rowSeq = pData(data)$rowSeq,
+                            colSeq = pData(data)$colSeq, 
+                            x      = data[[aXaxisLabel]] , 
+                            y      = data[['value']], 
+                            bOut   = data[['IsOutlier']])
   
+  aResult   <- ddply(aD, ~rowSeq + colSeq, .fun = operatorFunction, .progress=progress_win(title="Exposure Time Scaling"))
+  aResult   <- ddply(aResult, ~rowSeq, .fun = calFractionPresent)
+  aMetaData <- data.frame(labelDescription=c("rowSeq", "colSeq", "slope", "intercept", "R2", "nPoints", "Result", "fractionPresent"), 
+                          groupingType=c("rowSeq","colSeq" , "QuantitationType" , "QuantitationType", "QuantitationType", "QuantitationType","QuantitationType", "Spot") )
+  
+  return(new("AnnotatedDataFrame", data=aResult, varMetadata=aMetaData))
 }
 
 ctx <- tercenCtx()
