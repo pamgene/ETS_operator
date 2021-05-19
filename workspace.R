365698a0ac2dac1defb3dcd1720c1ff8ec2e3509
ginberg <info@gerinberg.com>
2021-05-19 18:26:23 +0000

    add predictions to output

--- workspace.R
+++ workspace.R
@@ -12,33 +12,29 @@ do.ETS <- function(data){
   if (nPoints > 1){      
     aLm <- try(lm(dataY ~ dataX+0), silent = TRUE)
     if(!inherits(aLm, 'try-error')){
-      slope = aLm$coefficients[[1]]
-      intercept = 0
-      ssY <- sum((dataY-mean(dataY))^2)
-      yFit <- predict(aLm)
-      R2 <- 1-(sum((dataY-yFit)^2) / ssY)
-      Result = 1;
-      
+      slope     <- aLm$coefficients[[1]]
+      intercept <- 0
+      ssY       <- sum((dataY-mean(dataY))^2)
+      yFit      <- predict(aLm)
+      R2        <- 1-(sum((dataY-yFit)^2) / ssY)
+      Result    <- 1
     } else {
-      slope = NaN
-      intercept = NaN
-      R2 = NaN
-      Result = 0;
+      slope  <- intercept <- R2 <- yFit <- NaN
+      Result <- 0
     }
   } 
   if (nPoints == 1){
-    slope = dataY/dataX
-    intercept = 0;
-    R2 = 1
-    Result = 1
+    slope     <- dataY/dataX
+    intercept <- 0
+    R2        <- 1
+    Result    <- 1
+    yFit      <- dataY
   }
   if (nPoints == 0){
-    slope = NaN
-    intercept = NaN
-    R2 = NaN
-    Result = 0;
+    slope  <- intercept <- R2 <- yFit <- NaN
+    Result <- 0
   }
-  data.frame(slope=slope, intercept = intercept, R2 = R2, nPoints = nPoints, Result = Result)
+  data.frame(slope=slope, intercept = intercept, R2 = R2, nPoints = nPoints, Result = Result, xFit = dataX, yFit = yFit)
 }
 
 calFractionPresent <- function(data){
@@ -50,12 +46,21 @@ ctx <- tercenCtx()
 if(inherits(try(ctx$select(".x")), 'try-error')) stop("x axis is missing.")
 if(inherits(try(ctx$select(".y")), 'try-error')) stop("y axis is missing.")
 
-ctx %>% 
-  select(.x, .y, .ri, .ci) %>% 
+ETS_result <- ctx %>% 
+  select(.x, .y, .ri, .ci) %>%
   group_by(.ri, .ci) %>%
-  do(do.ETS(.)) %>%
+  do(do.ETS(.)) 
+
+sum.table <- ETS_result %>%
+  select(-c(xFit, yFit)) %>%
+  unique() %>% 
   ungroup() %>%
   group_by(.ri, .ci) %>%
   do(calFractionPresent(.)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+  ctx$addNamespace() 
+
+pred.table <- ETS_result %>%
+  select(.ri, .ci, xFit, yFit) %>%
+  ctx$addNamespace()
+
+ctx$save(list(sum.table, pred.table))
