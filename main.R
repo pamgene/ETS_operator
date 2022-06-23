0236a322939aaa4dc6cea5b5a55440398f0c2cd0
agouy <contact@agouy.com>
2022-06-23 07:40:26 +0000

    .ri and .ci order

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 library(tercen)
 library(dplyr)
 library(dtplyr)
+library(data.table)
 
 ctx <- tercenCtx()
 
@@ -11,47 +12,46 @@ dt_in <- ctx %>%
   data.table::as.data.table()
 
 ETS_result <- dt_in[, 
-          {
-            x = .x
-            y = .y
-            
-            nPoints = length(x)
-            if (nPoints > 1) {
-              slope     <- sum(x*y) / sum(x^2)
-              intercept <- 0
-              ssY       <- sum((y-mean(y))^2)
-              yFit      <- x * slope
-              R2        <- 1-(sum((y-yFit)^2) / ssY)
-              Result    <- 1
-              
-            } 
-            if (nPoints == 1) {
-              slope     <- y/x
-              intercept <- 0
-              R2        <- 1
-              Result    <- 1
-              yFit      <- y
-            }
-            if (nPoints == 0) {
-              slope  <- intercept <- R2 <- yFit <- NaN
-              Result <- 0
-            }
-            list(
-              slope = slope,
-              intercept = intercept,
-              R2 = R2,
-              nPoints = as.double(nPoints),
-              Result = Result,
-              xFit = x,
-              yFit = yFit
-            )
-          }, by=c(".ri", ".ci")
+  {
+    x = .x
+    y = .y
+    
+    nPoints = length(x)
+    if (nPoints > 1) {
+      slope     <- sum(x*y) / sum(x^2)
+      intercept <- 0
+      ssY       <- sum((y-mean(y))^2)
+      yFit      <- x * slope
+      R2        <- 1-(sum((y-yFit)^2) / ssY)
+      Result    <- 1
+    } 
+    if (nPoints == 1) {
+      slope     <- y/x
+      intercept <- 0
+      R2        <- 1
+      Result    <- 1
+      yFit      <- y
+    }
+    if (nPoints == 0) {
+      slope  <- intercept <- R2 <- yFit <- NaN
+      Result <- 0
+    }
+    list(
+      slope = slope,
+      intercept = intercept,
+      R2 = R2,
+      nPoints = as.double(nPoints),
+      Result = Result,
+      xFit = x,
+      yFit = yFit
+    )
+  }, by=c(".ri", ".ci")
 ]
 
 sum.table <- ETS_result %>%
   select(-c(xFit, yFit)) %>%
   unique() %>% 
-  ungroup() %>%
+  arrange(.ri, .ci) %>%
   group_by(.ri, .ci) %>%
   mutate(fractionPresent = mean(Result)) %>%
   as_tibble() %>%
